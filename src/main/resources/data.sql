0a227cc9f472b953b2825b04b0a4698943c37f06
jimyjcp <jimmyjcp@gmail.com>
2025-12-26 09:59:38 -0500

    agregar docker compose para app y mysql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO persona (nombre, apellido, email, telefono)
+VALUES ('Juan', 'Perez', 'juan@test.com', '123456789');
+
+INSERT INTO persona (nombre, apellido, email, telefono)
+VALUES ('Maria', 'Lopez', 'maria@test.com', '987654321');
